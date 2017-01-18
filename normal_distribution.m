e8e77e073a233a313661a21a4210d0550513978a
Filip Hallqvist <filiphallqvist@gmail.com>
2017-01-18 15:45:46 +0100

    Fixed stuffz

--- normal_distribution.m
+++ normal_distribution.m
@@ -3,36 +3,27 @@ hold on
 mean_ = [1; 1];
 cov_ = [0.1 -0.05; -0.05 0.2];
 
-points = arrayfun(@(x) transpose(mvnrnd(mean_, cov_)), 1:10, 'UniformOutput', false);
+points = arrayfun(@(x) transpose(mvnrnd(mean_, cov_)), 1:1000, 'UniformOutput', false);
 celldisp(points)
-% values = cellfun(@(p) f(p, 3), points, 'UniformOutput', false);
+values = cell2mat(cellfun(@(p) f(p, 3), points, 'UniformOutput', false));
 
-% insidePoints = points(f(points, 3) <= 0);
-% outsidePoints = points(f(points, 3) > 0);
-% 
-% insidePoints
-% outsidePoints
+insidePoints = points(find(values < 0))
+outsidePoints = points(find(values >= 0))
 
-% for n = 1:10
-%     p = points(n);
-%     
-%     if f(p, 3) <= 0
-%         insidePoints = [insidePoints, p];
-%     else
-%         outsidePoints = [outsidePoints, p];
-%     end
-% end
+ixs = cellfun(@(p) p(1), insidePoints);
+iys = cellfun(@(p) p(2), insidePoints);
 
-% xs = cellfun(@(p) p(1), points);
-% ys = cellfun(@(p) p(2), points);
-% 
-% limits = [min([xs ys]) max([xs ys])];
+oxs = cellfun(@(p) p(1), outsidePoints);
+oys = cellfun(@(p) p(2), outsidePoints);
+ 
+limits = [min([xs ys]) max([xs ys])];
 
-% ezplot(@(x,y) f([x;y], 1), limits);
-% ezplot(@(x,y) f([x;y], 2), limits);
-% ezplot(@(x,y) f([x;y], 3), limits);
+ezplot(@(x,y) f([x;y], 1), limits);
+ezplot(@(x,y) f([x;y], 2), limits);
+ezplot(@(x,y) f([x;y], 3), limits);
 
-% scatter(xs, ys);
+scatter(ixs, iys);
+scatter(oxs, oys);
 
 function y = f(x, r)
     mean_ = [1; 1];
