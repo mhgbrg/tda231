88b6481f77062990a5c0fba65c0997240290b5b8
Filip Hallqvist <filiphallqvist@gmail.com>
2017-01-18 16:00:06 +0100

    Finalized the kick-ass figure

--- normal_distribution.m
+++ normal_distribution.m
@@ -7,8 +7,8 @@ points = arrayfun(@(x) transpose(mvnrnd(mean_, cov_)), 1:1000, 'UniformOutput', 
 celldisp(points)
 values = cell2mat(cellfun(@(p) f(p, 3), points, 'UniformOutput', false));
 
-insidePoints = points(find(values < 0))
-outsidePoints = points(find(values >= 0))
+insidePoints = points(find(values <= 0))
+outsidePoints = points(find(values > 0))
 
 ixs = cellfun(@(p) p(1), insidePoints);
 iys = cellfun(@(p) p(2), insidePoints);
@@ -18,16 +18,29 @@ oys = cellfun(@(p) p(2), outsidePoints);
  
 limits = [min([xs ys]) max([xs ys])];
 
-ezplot(@(x,y) f([x;y], 1), limits);
-ezplot(@(x,y) f([x;y], 2), limits);
-ezplot(@(x,y) f([x;y], 3), limits);
+r1 = ezplot(@(x,y) f([x;y], 1), limits);
+r2 = ezplot(@(x,y) f([x;y], 2), limits);
+r3 = ezplot(@(x,y) f([x;y], 3), limits);
 
-scatter(ixs, iys);
-scatter(oxs, oys);
+set(r1, 'Color', 'red');
+set(r2, 'Color', 'magenta');
+set(r3, 'Color', 'cyan');
+
+scatter(ixs, iys, 40, 'blue');
+scatter(oxs, oys, 40, 'black');
+
+[~, outsidePointCount] = size(outsidePoints)
+
+
+caption = sprintf('The number of points outside r = 3 is %i', outsidePointCount);
+title(caption, 'FontSize', 14);
+
+hold off
+
+legend('r = 1','r = 2', 'r = 3')
 
 function y = f(x, r)
     mean_ = [1; 1];
     cov_ = [0.1 -0.05; -0.05 0.2];
-
     y = (transpose(x - mean_) * inv(cov_) * (x - mean_)) ./ 2 - r;
 end
