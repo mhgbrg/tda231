a3eea52c2ba5a0a6d1d1c7a545b1b5e3412bf64b
Mats Högberg <mats@hgbrg.se>
2017-01-18 17:35:18 +0100

    Problem 2.2

--- hw0/problem22.m
+++ hw0/problem22.m
@@ -1,8 +1,21 @@
 clear;
 
+hold on;
+
 X = dlmread('dataset0.txt');
 
 xCov = cov(X);
 xCor = corrcoef(X);
 
-Y = normc(X);
+Y = (X - min(X)) ./ (max(X) - min(X));
+
+surf(corrcoef(X))
+
+figure
+surf(corrcoef(Y))
+
+figure
+surf(cov(X))
+
+figure
+surf(cov(Y))
