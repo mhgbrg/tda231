01b6daba925d74990a913cf1ff127eeab2c28e61
Mats Högberg <mats@hgbrg.se>
2017-01-18 15:37:43 +0100

    Stuff

--- normal_distribution.m
+++ normal_distribution.m
@@ -1,18 +1,42 @@
+hold on
+
 mean_ = [1; 1];
 cov_ = [0.1 -0.05; -0.05 0.2];
 
-points = arrayfun(@(x) mvnrnd(mean_, cov_), 1:1000, 'UniformOutput', false);
+points = arrayfun(@(x) transpose(mvnrnd(mean_, cov_)), 1:10, 'UniformOutput', false);
+celldisp(points)
+% values = cellfun(@(p) f(p, 3), points, 'UniformOutput', false);
 
-xs = cellfun(@(p) p(1), points);
-ys = cellfun(@(p) p(2), points);
+% insidePoints = points(f(points, 3) <= 0);
+% outsidePoints = points(f(points, 3) > 0);
+% 
+% insidePoints
+% outsidePoints
 
-scatter(xs, ys)
+% for n = 1:10
+%     p = points(n);
+%     
+%     if f(p, 3) <= 0
+%         insidePoints = [insidePoints, p];
+%     else
+%         outsidePoints = [outsidePoints, p];
+%     end
+% end
 
-fplot(@(x) f(x, 1))
+% xs = cellfun(@(p) p(1), points);
+% ys = cellfun(@(p) p(2), points);
+% 
+% limits = [min([xs ys]) max([xs ys])];
+
+% ezplot(@(x,y) f([x;y], 1), limits);
+% ezplot(@(x,y) f([x;y], 2), limits);
+% ezplot(@(x,y) f([x;y], 3), limits);
+
+% scatter(xs, ys);
 
 function y = f(x, r)
     mean_ = [1; 1];
     cov_ = [0.1 -0.05; -0.05 0.2];
 
-    y = (transpose(x - mean_) * inv(cov_) * (x - mean_)) / 2 - r;
+    y = (transpose(x - mean_) * inv(cov_) * (x - mean_)) ./ 2 - r;
 end
