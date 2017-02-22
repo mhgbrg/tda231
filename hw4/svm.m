ff862cc4108b4b2f295fb905ff975e057e3db6f7
Filip Hallqvist <filiphallqvist@gmail.com>
2017-02-22 14:48:39 +0100

    deoajnd

--- hw4/svm.m
+++ hw4/svm.m
@@ -1,14 +1,27 @@
 clear;
+fig = figure
+hold on
 
 data = load('d1b.mat');
 
 X = data.X;
 Y = data.Y;
 
-% X1 = X(Y == 1, :);
-% X2 = X(Y == -1, :);
-% scatter(X1(:,1), X1(:,2), 30, 'x');
-% scatter(X2(:,1), X2(:,2), 30, 'o');
+X1 = X(Y == 1, :);
+X2 = X(Y == -1, :);
+scatter(X1(:,1), X1(:,2), 30, 'x');
+scatter(X2(:,1), X2(:,2), 30, 'o');
 
-SVMStruct = svmtrain(X, Y, 'ShowPlot', true, 'BoxConstraint', 1)
+SVMStruct = svmtrain(X, Y, 'BoxConstraint', 10000)
 
+
+ind = SVMStruct.SupportVectorIndices;
+a = SVMStruct.Alpha;
+b = SVMStruct.Bias;
+
+w = [0; 0];
+for n=1:size(a)
+    w = w + a(n) * Y(ind(n)) * transpose(X(ind(n),:));
+end
+
+ezplot(@(x1, x2) w(1)*x1 + w(2)*x2)
