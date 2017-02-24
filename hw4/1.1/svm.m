c5222118e9bf7ff521d97ebe3da64a6adf1fc549
Mats Högberg <mats@hgbrg.se>
2017-02-24 21:36:06 +0100

    Fixed plot

--- hw4/1.1/svm.m
+++ hw4/1.1/svm.m
@@ -15,8 +15,8 @@ scatter(X1(:,1), X1(:,2), '+', 'red');
 scatter(X2(:,1), X2(:,2), 'x', 'blue');
 scatter(S(:,1), S(:,2), 50, 'o', 'black');
 ezplot(@(x1, x2) x1 + x2 - 3);
-xlim([-1 5]);
-ylim([-1 5]);
+xlim([-1 6]);
+ylim([-1 6]);
 title('SVM');
 legend('+1', '-1', 'Support vectors', 'Decision boundary');
 
