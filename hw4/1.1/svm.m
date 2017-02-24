f83f7277455c9fb2cf429400ac0ef9cc724a7298
Mats Högberg <mats@hgbrg.se>
2017-02-24 21:35:05 +0100

    Added support vectors to plot

--- hw4/1.1/svm.m
+++ hw4/1.1/svm.m
@@ -4,15 +4,20 @@ X1 = [2 2;
 X2 = [0 0;
       2 0;
       0 2];
+S = [2 2;
+     4 0;
+     2 0;
+     0 2];
 
 fig = figure;
 hold on;
 scatter(X1(:,1), X1(:,2), '+', 'red');
 scatter(X2(:,1), X2(:,2), 'x', 'blue');
+scatter(S(:,1), S(:,2), 50, 'o', 'black');
 ezplot(@(x1, x2) x1 + x2 - 3);
 xlim([-1 5]);
 ylim([-1 5]);
 title('SVM');
-legend('+1', '-1', 'Decision boundary');
+legend('+1', '-1', 'Support vectors', 'Decision boundary');
 
 saveas(fig, '../figures/problem1.eps', 'epsc');
