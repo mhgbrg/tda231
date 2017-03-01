04a4509f73ce61eef0bac3486768c4384dd51b36
Mats Högberg <mats@hgbrg.se>
2017-03-01 15:50:14 +0100

    Different markers for scatter

--- hw5/1.2/visualize.m
+++ hw5/1.2/visualize.m
@@ -1,21 +1,26 @@
 clear;
 addpath('./tSNE_matlab');
 
+samples = 1000;
+k = 10;
+
 data = load('medium_100_10k.mat');
 
 vocab = data.vocab;
 embeds = data.wordembeddings;
 
-[ y, ind ] = datasample(vocab, 1000);
+[ y, ind ] = datasample(vocab, samples);
 X = embeds(ind,:);
 
 X_ = tsne(X);
 
-C = kmeans(X, 10);
+C = kmeans(X, k);
+
+markers = ['o', '+', '*', 'x', 's', 'd', '^', 'v', 'p', 'h'];
 
 figure;
 hold on;
-for i=1:10
+for i=1:k
     X_i = X_(C == i, :);
-    scatter(X_i(:,1), X_i(:,2));
+    scatter(X_i(:,1), X_i(:,2), markers(i));
 end
