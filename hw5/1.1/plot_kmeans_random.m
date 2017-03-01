e8be3be0cdf263bf4731e0b41ba3cafed360e17d
Mats Högberg <mats@hgbrg.se>
2017-03-01 16:01:29 +0100

    Added figure to 1.1

--- hw5/1.1/plot_kmeans_random.m
+++ hw5/1.1/plot_kmeans_random.m
@@ -8,7 +8,10 @@ X1 = X(y == 1,:);
 X2 = X(y == 2,:);
 X_ = X(y~=after2,:);
 
+fig = figure;
 hold on;
 scatter(X1(:,1), X1(:,2), 'x');
 scatter(X2(:,1), X2(:,2), '+');
 scatter(X_(:,1), X_(:,2), 50, 'ko');
+
+saveas(fig, '../figures/problem11b.eps', 'epsc');
