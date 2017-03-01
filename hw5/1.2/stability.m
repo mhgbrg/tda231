e1e116356ca4c29f035349f74041c215c2b98cbc
Filip Hallqvist <filiphallqvist@gmail.com>
2017-03-01 15:28:03 +0100

    Run experiments ten times

--- hw5/1.2/stability.m
+++ hw5/1.2/stability.m
@@ -5,40 +5,45 @@ data = load('medium_100_10k.mat');
 vocab = data.vocab;
 embeds = data.wordembeddings;
 
-% Run kmeans
-C = kmeans(embeds, 10, 'Replicates', 1);
+f = 0;
+runs = 10;
 
-% Calculate N0
-cavalry_class = C(strcmp(vocab, 'cavalry'));
-C1 = find(C == cavalry_class);
-C1_size = size(C1, 1);
+for p=1:runs
+    % Run kmeans
+    C = kmeans(embeds, 10, 'Replicates', 1);
 
-N0 = (C1_size^2 - C1_size) / 2;
+    % Calculate N0
+    cavalry_class = C(strcmp(vocab, 'cavalry'));
+    C1 = find(C == cavalry_class);
+    C1_size = size(C1, 1);
 
-% Run kmeans again
-C_ = kmeans(embeds, 10, 'Replicates', 1);
+    N0 = (C1_size^2 - C1_size) / 2;
 
-% Calculate N1
-N1 = 0;
+    % Run kmeans again
+    C_ = kmeans(embeds, 10, 'Replicates', 1);
 
-for i=1:C1_size
-    
-    word1_index = C1(i);
-    word1_class = C_(word1_index);
-    
-    for j=(i+1):C1_size
-        
-        word2_index = C1(j);
-        word2_class = C_(word2_index);
-        
-        if word1_class == word2_class
-            N1 = N1 + 1;
+    % Calculate N1
+    N1 = 0;
+
+    for i=1:C1_size
+
+        word1_index = C1(i);
+        word1_class = C_(word1_index);
+
+        for j=(i+1):C1_size
+
+            word2_index = C1(j);
+            word2_class = C_(word2_index);
+
+            if word1_class == word2_class
+                N1 = N1 + 1;
+            end
+
         end
-        
+
     end
-    
+
+    f = f + N1 / N0;
 end
 
-N0
-N1
-f = N1 / N0
+disp(f / runs);
