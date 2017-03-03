0f5526354fdc05f04e855df0f595fca11f93d147
Mats Högberg <mats@hgbrg.se>
2017-03-03 09:27:40 +0100

    Cleaned up kmeans_random

--- hw5/1.1/kmeans_random.m
+++ hw5/1.1/kmeans_random.m
@@ -1,37 +1,45 @@
-function [ y, after2 ] = kmeans_random( X, k)
-%KMEANS_RANDOM Summary of this function goes here
-%   Detailed explanation goes here
-N = size(X, 1);
-y = zeros(N, 1);
-
-mu = zeros(k, 0);
-mu_new = datasample(X, k, 'Replace', false);
-
-j = 1;
-while ~isequal(mu, mu_new)
-    mu = mu_new;
-    for n=1:N
-        x = X(n,:);
-        distances = zeros(k, 1);
-        for i=1:k
-            muk = mu(i, :);
-            dist = norm(x - muk);
-            distances(i) = dist;
+function Y = kmeans_random(X, K, max_iterations)
+    if ~exist('max_iterations', 'var')
+        max_iterations = 100;
+    end
+    
+    % Initiate centers to K data points chosen uniformly at random.
+    mu = datasample(X, K, 'Replace', false);
+    
+    % Initiate Y to a certain size so that we don't need to resize it in
+    % every iteration. The unused elements are removed when the loop ends.
+    Y = cell(max_iterations, 1);
+    
+    N = size(X, 1);
+    for i=1:max_iterations
+        y = zeros(N, 1);
+        
+        % Assign each point to its closest center.
+        for n=1:N
+            x = X(n,:);
+            distances = zeros(K, 1);
+            for k=1:K
+                muk = mu(k,:);
+                dist = (x - muk) * transpose(x - muk);
+                distances(k) = dist;
+            end
+            [~, index] = min(distances);
+            y(n) = index;
         end
-        [~, index] = min(distances);
-        y(n) = index;
-    end
+        
+        % Check if assignments have changed since last iteration.
+        if i > 1 && isequal(y, Y{i-1})
+            Y(i:max_iterations) = [];
+            break;
+        end
+        
+        % Save the new assignments.
+        Y{i} = y;
 
-    for i=1:k
-        xk = X(y == i,:);
-        mu_new(i,:) = mean(xk);
+        % Calculate new centers.
+        for k=1:K
+            xk = X(y == k,:);
+            mu(k,:) = mean(xk);
+        end
     end
-    
-    if j == 2
-        after2 = y;
-    end
-    j = j + 1;
-    
-end
-
 end
--- hw5/1.1/plot_kmeans_random.m
+++ hw5/1.1/plot_kmeans_random.m
@@ -2,16 +2,19 @@ data = load('hw5_p1a.mat');
 X = data.X;
 k = 2;
 
-[ y, after2 ] = kmeans_random(X, k);
+Y = kmeans_random(X, k);
 
-X1 = X(y == 1,:);
-X2 = X(y == 2,:);
-X_ = X(y~=after2,:);
+y_2 = Y{2};
+y_final = Y{end};
+
+X1 = X(y_final == 1,:);
+X2 = X(y_final == 2,:);
+X_diff = X(y_final ~= y_2,:);
 
 fig = figure;
 hold on;
 scatter(X1(:,1), X1(:,2), 'x');
 scatter(X2(:,1), X2(:,2), '+');
-scatter(X_(:,1), X_(:,2), 50, 'ko');
+scatter(X_diff(:,1), X_diff(:,2), 50, 'ko');
 
 saveas(fig, '../figures/problem11b.eps', 'epsc');
