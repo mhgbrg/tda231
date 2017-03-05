09ca3c14c8d75903c5445e77b973c770028b374b
Mats Högberg <mats@hgbrg.se>
2017-03-05 16:49:00 +0100

    Added comments and use of scatter_clusters to 1.2/visualize.m

--- hw5/1.2/visualize.m
+++ hw5/1.2/visualize.m
@@ -1,28 +1,25 @@
 clear;
 addpath('./tSNE_matlab');
-
-samples = 1000;
-k = 10;
+addpath('../helpers');
 
 data = load('medium_100_10k.mat');
-
 vocab = data.vocab;
 embeds = data.wordembeddings;
 
-[ y, ind ] = datasample(vocab, samples);
-X = embeds(ind,:);
+samples = 1000;
+K = 10;
 
+% Sample dataset
+X = datasample(embeds, samples, 'Replace', false);
+
+% Classify data
+y = kmeans(X, K);
+
+% Project data to two dimensions using t-SNE
 X_ = tsne(X);
 
-C = kmeans(X, k);
-
-markers = ['o', '+', '*', 'x', 's', 'd', '^', 'v', 'p', 'h'];
-
+% Plot projected data points 
 fig = figure;
-hold on;
-for i=1:k
-    X_i = X_(C == i, :);
-    scatter(X_i(:,1), X_i(:,2), markers(i));
-end
+scatter_clusters(X_, y, K, '');
 
 saveas(fig, '../figures/problem12d.eps', 'epsc');
